d201ec906947868603cd178cb834e33c43c5c532
Marek Belski <marek.belski@gmail.com>
2024-10-28 14:54:03 -0700

    Has volume slider save to config file.

--- Motor/Basic/mappable-keys/src/options/volume_slider.gd
+++ Motor/Basic/mappable-keys/src/options/volume_slider.gd
@@ -1,12 +1,15 @@
 extends HSlider
 
-@export var bus_name: String = "Master"
+const AUDIO_CONFIG_SECTION = "Audio"
+
+@export var bus_name: StringName = "Master"
 # This path can link to an AudioStreamPlayer node to play after the volume has changed. 
 # For a music volume slider it should not be used (music should be playing while in the menu), 
 # but for a sounds volume slider it helps the user adjust to the right volume. 
 @export var feedback_sound_path: NodePath
 
 var _feedback_sound: AudioStreamPlayer = null
+var _original_bus_volume : float = 1.0
 
 var _bus_index = 0
 
@@ -14,15 +17,17 @@ func _ready() -> void:
 	if feedback_sound_path != null:
 		_feedback_sound = get_node(feedback_sound_path)
 	_bus_index = AudioServer.get_bus_index(bus_name)
-	_set_volume(value)
+	_original_bus_volume = db_to_linear(AudioServer.get_bus_volume_db(_bus_index))
+	var config_value = Config.get_config(AUDIO_CONFIG_SECTION, bus_name, 1.0)
+	set_value_no_signal(config_value)
+	_set_volume(config_value)
 
+func _set_volume(volume_linear: float) -> void:
+	AudioServer.set_bus_volume_db(_bus_index, linear_to_db(volume_linear * _original_bus_volume))
+	Config.set_config(AUDIO_CONFIG_SECTION, bus_name, volume_linear)
 
-func _set_volume(value: float) -> void:
-	AudioServer.set_bus_volume_db(_bus_index, linear_to_db(value))
-
-
-func _on_VolumeSlider_value_changed(value: float) -> void:
-	_set_volume(value)
+func _on_VolumeSlider_value_changed(new_value: float) -> void:
+	_set_volume(new_value)
 	if _feedback_sound != null:
 		if not _feedback_sound.is_playing():
 			_feedback_sound.play()
