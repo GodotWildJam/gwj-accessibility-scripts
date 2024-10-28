5430b61324a4cef929d7b58d09bf3a17cce03a38
Marek Belski <marek.belski@gmail.com>
2024-10-28 15:00:49 -0700

    Has mute setting save to config file.

--- Motor/Basic/mappable-keys/src/options/mute_button.gd
+++ Motor/Basic/mappable-keys/src/options/mute_button.gd
@@ -1,16 +1,21 @@
 extends CheckBox
 
+const AUDIO_CONFIG_SECTION = "Audio"
+const MUTE_BUS_PREFIX = "%sMute"
 
-@export var bus_name: String = "Master"
+@export var bus_name: StringName = "Master"
 
 var _bus_index = 0
 
 func _ready() -> void:
 	_bus_index = AudioServer.get_bus_index(bus_name)
-	_set_mute(false)
+	var config_value = Config.get_config(AUDIO_CONFIG_SECTION, MUTE_BUS_PREFIX % bus_name, false)
+	button_pressed = config_value
+	_set_mute(config_value)
 
-func _set_mute(value: bool) -> void:
-	AudioServer.set_bus_mute(_bus_index, value)
+func _set_mute(muted: bool) -> void:
+	AudioServer.set_bus_mute(_bus_index, muted)
+	Config.set_config(AUDIO_CONFIG_SECTION, MUTE_BUS_PREFIX % bus_name, muted)
 
 func _on_toggled(toggled_on):
 	_set_mute(toggled_on) # Replace with function body.
